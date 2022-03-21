c1362129c3ad25f43b56a665e338a3e5554c04a8
AlexGol005 <pesok1985>
2022-03-21 18:50:40 +0300

    21/03 pa

--- 21.3.py
+++ 21.3.py
@@ -1,1 +1,14 @@
-print('hello')
+dict_ = {}
+inFile = open('input.txt', 'r', encoding='utf8')
+outFile = open('output.txt', 'w', encoding='utf8')
+for line in inFile:
+    line = line.split()
+    if int(line[2]) not in dict_:
+        dict_[int(line[2])] = [int(line[3])]
+    else:
+        dict_[int(line[2])].append(int(line[3]))
+
+print(dict_)
+
+inFile.close()
+outFile.close()
