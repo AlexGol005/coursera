71ef7b6d064d47538c619cb3f40888e57aa30def
AlexGol005 <pesok1985>
2022-03-23 17:57:40 +0300

    23/03 pa

--- 3.22.1.py
+++ 3.22.1.py
@@ -1,29 +1,28 @@
 class Man:
     surname = ''
     name = ''
-    school_num = 0
+    sch_num = 0
     middle_grade = 0
 
-def manKey(man):
-    return (man.surname, man.name, man.school_num, man.middle_grade)
 
-n = int(input())
+def manKey(man):
+    return (man.surname, man.name, man.sch_num, man.middle_grade)
+
+
 peopleList = []
-for i in range(n):
-    tempManData = input().split()
+inFile = open('input.txt', 'r', encoding='utf8')
+outFile = open('output.txt', 'w', encoding='utf8')
+for line in inFile:
+    tempManData = line.split()
     man = Man()
-    man.height = int(tempManData[0])
+    man.surname = tempManData[0]
     man.name = tempManData[1]
+    man.sch_num = tempManData[2]
+    man.middle_grade = tempManData[3]
     peopleList.append(man)
 peopleList.sort(key=manKey)
 for man in peopleList:
-    print(man.height, man.name)
-
-
-inFile = open('input.txt', 'r', encoding='utf8')
-outFile = open('output.txt', 'w', encoding='utf8')
-
-
+    print(man.surname, man.name, man.sch_num, man.middle_grade, file=outFile)
 
 inFile.close()
 outFile.close()
